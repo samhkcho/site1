c6c2e615f3b8c35a130fa3f8369a8b1916000bb5
Sam <samhkcho@gmail.com>
2022-10-28 04:33:39 -0400

    redo routes

--- src/App.js
+++ src/App.js
@@ -20,14 +20,15 @@ export default function App() {
       <Routes>
         <Route path="/beta" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/beta/about" element={<About />} />
-          <Route path="/beta/dashboard" element={<Dashboard />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
 
           {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
           <Route path="*" element={<NoMatch />} />
         </Route>
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
   );
