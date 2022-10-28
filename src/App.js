e074b58d7b6002d8f5d233c18d5148f3a05e91f0
Sam <samhkcho@gmail.com>
2022-10-28 04:30:13 -0400

    add beta routes

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ export default function App() {
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/beta" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="/beta/about" element={<About />} />
+          <Route path="/beta/dashboard" element={<Dashboard />} />
 
           {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
